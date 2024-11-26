b635d3aec6bb285c94165cf5b3be85033b884864
mochki <grapefruitnights@protonmail.com>
2024-11-26 16:34:38 -0700

    tracking changes on diff sizes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -11,7 +11,7 @@ export const Menu: React.FC<{ show: boolean }> = ({ show }) => (
         {pageMetadata.map(({ id, title }) => (
           <div
             key={id}
-            className="flex text-center text-gray-600 hover:tracking-[-1.15em] hover:text-white hover:ease-in-out duration-100"
+            className="flex text-center text-gray-600 duration-100 hover:ease-in-out hover:text-white hover:tracking-[-0.5em] md:hover:tracking-[-0.8em] 2xl:hover:tracking-[-1.15em]"
           >
             <Link
               className="!leading-[0.7] text-7xl sm:text-8xl lg:text-9xl 2xl:text-[10rem] w-full text-nowrap overflow-hidden"
